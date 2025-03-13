27a8ee461264a16b61424ad8fcdd424ddf38dc93
omarhuanca <oma378501@yahoo.com>
2025-03-12 22:42:28 -0400

    INV-2: Add test for coin, price  - Modify: change use variables on coin.test.ts  - Modify: change use variables on price.test.ts

--- test/business/price/price.test.ts
+++ test/business/price/price.test.ts
@@ -3,38 +3,38 @@ import { Price } from "../../../src/business/price/model/Price"
 
 describe("Price suit validation", () => {
     test("verifyCompareValueSuccess", () => {
-        let coin = new Coin(Coin.COIN_USD);
-        let price = new Price(15.2, coin);
+        const coin = new Coin(Coin.COIN_USD);
+        const price = new Price(15.2, coin);
 
         expect(price.compareOtherValue(14)).toBeFalsy();
     });
     test("verifyCompareValueFailure", () => {
-        let coin = new Coin(Coin.COIN_USD);
-        let price = new Price(15.9, coin);
+        const coin = new Coin(Coin.COIN_USD);
+        const price = new Price(15.9, coin);
 
         expect(price.compareOtherValue(15.9)).toBeTruthy();
     });
     test("verifyValueNegativeFalse", () => {
-        let coin = new Coin(Coin.COIN_USD);
-        let price = new Price(15.9, coin);
+        const coin = new Coin(Coin.COIN_USD);
+        const price = new Price(15.9, coin);
 
         expect(price.isNegativeValue()).toBeFalsy();
     });
     test("verifyValueNegativeTrue", () => {
-        let coin = new Coin(Coin.COIN_USD);
-        let price = new Price(-25, coin);
+        const coin = new Coin(Coin.COIN_USD);
+        const price = new Price(-25, coin);
 
         expect(price.isNegativeValue()).toBeTruthy();
     });
     test("verifyCompareCodeFailure", () => {
-        let coin = new Coin(Coin.COIN_USD);
-        let price = new Price(15.9, coin);
+        const coin = new Coin(Coin.COIN_USD);
+        const price = new Price(15.9, coin);
 
         expect(price.compareOtherCode(Coin.COIN_BS)).toBeFalsy();
     });
     test("verifyCompareCodeSuccess", () => {
-        let coin = new Coin(Coin.COIN_USD);
-        let price = new Price(15.9, coin);
+        const coin = new Coin(Coin.COIN_USD);
+        const price = new Price(15.9, coin);
 
         expect(price.compareOtherCode(Coin.COIN_USD)).toBeTruthy();
     });

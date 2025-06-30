5759a8aa0945185f7a5bda693dc16dae732af585
omarhuanca <oma378501@yahoo.com>
2025-06-30 10:16:20 -0400

    INV-6: Add simple product  - Modify: add call endpoint on ProductClient.ts  - Modify: add iteration products on ProductList.tsx  - Modify: add structure product on ProductComponent.tsx  - Add: add default image on defaultImgeProduct.jpg  - Remove: remove card component on card.tsx  - Modify: change read main component App.tsx

---
+++ src/business/product/api/ProductList.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+import ProductClient from './ProductClient';
+import ProductComponent from '../component/ProductComponent';
+import Box from '@mui/material/Box';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        ProductClient.get('v1/products')
+          .then(response => {
+            setProducts(response.data);
+            setLoading(false);
+          })
+          .catch(error => {
+            setError(error.message || 'Unknow error');
+            setLoading(false);
+          })
+    }, []);
+
+    if (loading)
+      return <>Loading products....</>
+    if (error)
+      return <>Error: {error}</>
+
+    return (
+        <>
+          <h1>Productos</h1>
+          <Box sx={{
+            width: '100%',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(min(200px, 100%), 1fr))',
+            gap: 2,
+          }}>
+            {products.map(product => (
+              <ProductComponent key={product.code} product={product} />
+            ))}
+          </Box>
+        </>
+    )
+};
+
+export default ProductList;

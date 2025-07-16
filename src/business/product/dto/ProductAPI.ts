94c6d4b63ecc406ad74294ca5b3671b3c361e6ca
omarhuanca <oma378501@yahoo.com>
2025-07-16 09:45:21 -0400

    INV-7: Add image product  - Modify: remove timout on ProductClient.ts  - Modify: add measurement on ProductComponent.tsx  - Modify: add measurement field on ProductAPI.ts

--- src/business/product/dto/ProductAPI.ts
+++ src/business/product/dto/ProductAPI.ts
@@ -1,8 +1,10 @@
 import { CoinAPI } from "../../coin/dto/CoinAPI";
+import { MeasurementAPI } from "../../measurement/dto/MeasurementAPI";
 
 export interface ProductAPI {
     code: string;
     description: string;
+    measurement: MeasurementAPI;
     priceSale: number;
     imageBase64: string;
     coin: CoinAPI;

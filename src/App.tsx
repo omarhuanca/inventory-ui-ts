f35d8949a60791f50b4acdee15947ca650810bcb
omarhuanca <oma378501@yahoo.com>
2026-01-11 14:49:09 -0400

    INV-9: Add location store  -Modify: add component Footer.tsx  -Modify: add component footer in ProductList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css'
 import ProductList from './business/product/api/ProductList'
+import Footer from './page/Footer';
 
 function App() {
-  return <ProductList />;
+  return (
+    <>
+     <ProductList />
+     <Footer/>
+    </>
+  );
 
 }
 
